d85a4c3f3c55b015c7b0bc7a8d9ff88ede3b4afc
Gregg Virostek <gregg@virostek.me>
2018-04-30 16:23:29 +0100

    add link text changes
    link text is identified by a class within a span, updated attempts to capture and embiggen it

--- sboExt.js
+++ sboExt.js
@@ -2,12 +2,20 @@
 // MIT license
 // ----------------------------------------------------------
 
-// store the monospaced span elements in an HTML collection
+// store the monospaced body text in an HTML collection
 let e = document.getElementsByTagName('kbd');
 
+// store the monospaced link text in an HTML collection
+let l = document.getElementsByClassName('URLPACKT');
 
-// iterate over the collection and change fontSize of all elements
+
+// iterate over the body text collection and change fontSize of all elements
 for (let i = 0; i < e.length; i++) {
   e[i].style.fontSize = "1.188em";
+}
+
+// iterate over the link collection and change fontSize of all elements
+for (let i = 0; i < l.length; i++) {
+  l[i].style.fontSize = "1.188em";
 }
 
