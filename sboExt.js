04da1d48613ef6faa070545f1422accfa9fd2561
Gregg Virostek <gregg@virostek.me>
2019-11-05 08:15:32 -0500

    comment out link-text portion
    debugging plugin and need to simplify logic

--- sboExt.js
+++ sboExt.js
@@ -6,7 +6,7 @@
 let e = document.getElementsByTagName('kbd');
 
 // store the monospaced link text in an HTML collection
-let l = document.getElementsByClassName('URLPACKT');
+// let l = document.getElementsByClassName('URLPACKT');
 
 
 // iterate over the body text collection and change fontSize of all elements
@@ -15,7 +15,8 @@ for (let i = 0; i < e.length; i++) {
 }
 
 // iterate over the link collection and change fontSize of all elements
+/* 
 for (let i = 0; i < l.length; i++) {
   l[i].style.fontSize = "1.188em";
 }
-
+*/
